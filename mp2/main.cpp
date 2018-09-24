04c0daac9b65ac5eb984d8316aa0ff23b6a29c77
Steven Lee <solee2@siebl-0224-04.ews.illinois.edu>
2018-09-23 23:04:56 -0500

    Done except for leaks

--- mp2/main.cpp
+++ mp2/main.cpp
@@ -1,14 +1,16 @@
 #include "Image.h"
 #include "StickerSheet.h"
-
+#include <iostream>
 int main() {
 
   //
   // Reminder:
   //   Before exiting main, save your creation to disk as myImage.png
   //
+
 	Image meme;
-	meme.readFromFile("meme.png");
+	meme.readFromFile("meme1.resized.png");
+//	meme.readFromFile("meme.png");
 	Image statement;
 	statement.readFromFile("statement.png");
 	Image valgrind;
@@ -17,10 +19,12 @@ int main() {
 	gdb.readFromFile("gdb.png");
 
 	StickerSheet my_stickers(meme, 3);
-	my_stickers.addSticker(statement, 80, 400);
-	my_stickers.addSticker(valgrind, 500, 500);
-	my_stickers.addSticker(gdb, 500, 350);
-
+	my_stickers.addSticker(statement, 200, 600);
+	my_stickers.addSticker(valgrind, 830, 700);
+	my_stickers.addSticker(gdb, 845, 400);
+//	my_stickers.addSticker(statement, 20, 250);
+//	my_stickers.addSticker(valgrind, 250, 250);
+//	my_stickers.addSticker(gdb, 250, 250);
 	my_stickers.render().writeToFile("myImage.png");
 
   return 0;
