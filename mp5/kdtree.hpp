bcdfa27f091ae68f99c6d5518de9ca68b4f5c137
Steven Lee <solee2@linux-14.ews.illinois.edu>
2018-11-01 00:05:48 -0500

    Finish first 2 functions

--- mp5/kdtree.hpp
+++ mp5/kdtree.hpp
@@ -5,6 +5,7 @@
 
 #include <utility>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -15,7 +16,26 @@ bool KDTree<Dim>::smallerDimVal(const Point<Dim>& first,
     /**
      * @todo Implement this function!
      */
-
+// get first and second points that you are comparing
+	int valFirst = first[curDim];
+	int valSecond = second[curDim];
+// compare the values
+	if (valFirst < valSecond){
+		return true;
+	}
+	else if (valFirst > valSecond){
+		return false;
+	}
+// if they're equal, use < operator
+	else if (valFirst == valSecond){
+		if (first < second){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
     return false;
 }
 
@@ -27,7 +47,30 @@ bool KDTree<Dim>::shouldReplace(const Point<Dim>& target,
     /**
      * @todo Implement this function!
      */
+	double currentDistance = 0;
+	double potentialDistance = 0;
+// calculate currentBest's distance	
+	for (int i = 0; i < Dim; i++){
+		currentDistance = currentDistance + pow(target[i] - currentBest[i], 2);
+		potentialDistance = potentialDistance + pow(target[i] - potential[i], 2);
+	}
+	currentDistance = sqrt(currentDistance);
+	potentialDistance = sqrt(potentialDistance);
+// check if distances are equal
+// if so, compare the points
 
+// if distances are not equal
+	if (potentialDistance < currentDistance){
+		return true;
+	} 
+	else if (currentDistance < potentialDistance){
+		return false;
+	}
+	else if (currentDistance == potentialDistance){
+		return potential < currentBest;
+	}
+
+	
      return false;
 }
 
@@ -37,6 +80,9 @@ KDTree<Dim>::KDTree(const vector<Point<Dim>>& newPoints)
     /**
      * @todo Implement this function!
      */
+	
+
+
 }
 
 template <int Dim>
